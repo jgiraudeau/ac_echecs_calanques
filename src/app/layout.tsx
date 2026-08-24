7162b5c471dfa6589f578de8ca1ab9d0eab444d7
com-cassis <communication@cassisechecs.fr>
2026-08-24 22:00:09 +0200

    Ajout de la balise noscript de Google Tag Manager dans le body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -137,6 +137,14 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         />
       </head>
       <body className="antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WGSHKZLN"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdData) }}
