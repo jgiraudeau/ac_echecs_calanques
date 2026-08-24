a29fb4a1ec57524863f7dfd31c32cc108aee2655
com-cassis <communication@cassisechecs.fr>
2026-08-24 21:59:02 +0200

    Intégration de Google Tag Manager dans le layout et correction orthographique de COC pour le SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,7 +84,7 @@ const jsonLdData = {
     },
     {
       "@type": "SportsActivityLocation",
-      "name": "Club d'Échecs Carnoux - Club au COQ",
+      "name": "Club d'Échecs Carnoux - Club au COC",
       "address": {
         "@type": "PostalAddress",
         "streetAddress": "Av. Paul Cézanne",
@@ -125,6 +125,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WGSHKZLN');`
+          }}
+        />
+      </head>
       <body className="antialiased">
         <script
           type="application/ld+json"
